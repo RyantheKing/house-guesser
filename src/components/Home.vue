af763b4ecc5cdc00002379fea8e1d97c1b89eea2
Ryan Thomas King <rytking@ucsc.edu>
2023-04-03 20:19:34 -0700

    remove vue helloworld stuff

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Home">
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+
+export default class Home extends Vue {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
